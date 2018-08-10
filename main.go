15505b658480cd30850c25b113e4cc8ffaf2e48e
crystaldust <juzhenatpku@gmail.com>
2018-08-10 18:03:26 +0800

    Init commit

---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/upload", handleUpload)
+
+	fs := http.FileServer(http.Dir("testresults"))
+	mux.Handle("/", fs)
+	// mux.Handle("/", http.StripPrefix("/", fs))
+
+	http.ListenAndServe(":8883", mux)
+}
+
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	testname := r.Header.Get("testname")
+	if testname == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("testname not specified in headers"))
+		return
+	}
+
+	nowStr := time.Now().Format("20060102150405")
+	targetFileDir := fmt.Sprintf("./testresults/%s/%s", testname, nowStr)
+	_, err := os.OpenFile(targetFileDir, os.O_WRONLY, os.ModePerm)
+	if err != nil && !os.IsExist(err) {
+		os.MkdirAll(targetFileDir, os.ModePerm)
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	// targetFilePath := fmt.Sprintf("%s/%s", targetFileDir, "zipcontent")
+	// file, _ := os.Create(targetFilePath)
+	// n, err := io.Copy(file, r.Body)
+	// if err != nil {
+	//     fmt.Println("failed to copy file")
+	//     w.WriteHeader(http.StatusInternalServerError)
+	//     w.Write([]byte(err.Error()))
+	//     return
+	// }
+
+	if err := unzipToFile(r.Body, targetFileDir); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	// returnStr := fmt.Sprintf("%d bytes saved", n)
+	// w.Write([]byte(returnStr))
+}
+
+func unzipToFile(reqBody io.ReadCloser, targetFileDir string) error {
+	gzipReader, err := gzip.NewReader(reqBody)
+	if err != nil {
+		return err
+	}
+
+	tarReader := tar.NewReader(gzipReader)
+
+	for {
+		header, err := tarReader.Next()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		name := header.Name
+
+		target := filepath.Join(targetFileDir, header.Name)
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if e := os.MkdirAll(target, os.ModePerm); e != nil {
+				fmt.Println(e)
+			}
+		case tar.TypeReg:
+			file, _ := os.Create(target)
+			if _, e := io.Copy(file, tarReader); e != nil {
+				fmt.Println(e)
+			}
+		default:
+			fmt.Printf("%s : %c %s %s\n",
+				"Yikes! Unable to figure out type",
+				header.Typeflag,
+				"in file",
+				name,
+			)
+			return fmt.Errorf("Unable to figure out type")
+		}
+
+	}
+
+	return nil
+}
---
+++ upload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+upload_server="upload-server.servicecomb"
+rm -rf ./dashboard
+
+if [ "$REPORT_UPLOAD_SERVER" != "" ]; then
+	echo "Set upload_server to " $REPORT_UPLOAD_SERVER
+	upload_server=$REPORT_UPLOAD_SERVER
+fi
+
+echo $upload_server
+
+now_str=$(date +"%Y-%m-%d-%H-%M-%S")
+echo $now_str
+jmeter -g ./result.jtl -o dashboard
+tar zcf ${now_str}_upload.tgz ./result.jtl ./dashboard/
+curl -X POST --data-binary @./"${now_str}"_upload.tgz -H 'testname: spring-demo' http://$upload_server/upload
